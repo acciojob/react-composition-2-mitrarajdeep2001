4a82e0bb7c6ce865628bb49f9721cc7059cb5e53
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-06-15 12:23:12 +0000

    Third commit

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Modal({show, onClose}) {
     return(
-        <div className="modal-overlay">
+        <div className="modal">
             {
-                show && <div className="modal">
+                show && <div className="modal-overlay">
                 <button className="modal-close" onClick={onClose}>Close</button>
                 <p>This is the content of the modal.</p>
                 </div>
